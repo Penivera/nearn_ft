ce52aff3ca3308fe12f5de2ce9132c74b80b4ada
Penivera <penivera655@gmail.com>
2025-10-08 00:10:09 +0100

    setting up route for token transfer

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
 use actix_web::{post,HttpResponse,Responder};
+use near_sdk::AccountId;
+use serde::{Deserialize,Serialize};
+use std::env;
+use dotenv::dotenv;
+
+#[derive(Deserialize,Serialize)]
+struct TokenTransferRequest{
+    reciever_id:String,
+    amount:String,
+    memo:Option<String>
+}
+
+pub struct AccountConfig{
+    account_id:AccountId,
+    private_key:String,
+
+}
+
+impl AccountConfig{
+    pub fn new()->Self{
+        dotenv().ok();
+        Self{
+            account_id: env::var("NEAR_ACCOUNT_ID").expect("NEAR_ACCOUNT_ID NOT FOUND"),
+            private_key:env::var("NEAR_PRIVATE_KEY").expect("NEAR_PRIVATE_KEY NOT FOUND")
+
+        } 
+    }
+}
 
 
-#[post("/ft_transfer")]
+#[post("/transfer")]
 async fn ft_transfer() -> impl Responder {
     HttpResponse::Ok().body("coming soon")
+
 }
